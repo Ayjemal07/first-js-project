8f413aace564684edf52bf84fdcc593d071513d9
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2023-08-08 15:32:47 -0700

    For loop exercise with Pokemon Arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 let pokemonList =[]
-pokemonList=[{name:"Bulbasaur",height:7,types:["grass","poison"]},
-{name:"Charizard",height:2,types:["fire","flying"]},
-{name:"Parasect",height:1,types:["grass","bug"]}];
+pokemonList=[
+{name:"Bulbasaur",height:0.7,types:["grass","poison"]},
+{name:"Charizard",height:1.7,types:["fire","flying"]},
+{name:"Parasect",height:1.4,types:["grass","bug"]}
+]
+
+for (let i=0;i < pokemonList.length; i++){
+    text=pokemonList[i].name +" (height: "+ pokemonList[i].height+")."
+    if (pokemonList[i].height<1){
+      document.write(text+" It's a small pokemon "+"<br>");
+    }
+    else if (pokemonList[i].height > 1 && pokemonList[i].height < 1.5){
+      document.write(text+" It's an average size pokemon "+"<br>");
+    }
+    else{
+      document.write(text+" It's a big pokemon "+"<br>");
+    }
+}
