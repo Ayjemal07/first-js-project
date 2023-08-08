06de6ed03f7f5ed2c745afa0e6d24bd5cbbd209c
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2023-08-07 19:43:00 -0700

    made syntax fixes to the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,4 @@
 let pokemonList =[]
-pokemonList=[{name:"Bulbasaur",height:7,types:["grass","poison"]},{name:"Charizard",height:2,types:["fire","flying"]},{name:"Parasect",height:1,types:["grass","bug"]}]
+pokemonList=[{name:"Bulbasaur",height:7,types:["grass","poison"]},
+{name:"Charizard",height:2,types:["fire","flying"]},
+{name:"Parasect",height:1,types:["grass","bug"]}];
