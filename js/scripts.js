d0f220f53fd064331f761413f1427d257f94b09e
Ayjemal07 <125630176+Ayjemal07@users.noreply.github.com>
2023-08-13 21:59:26 -0700

    iife practice and foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,70 @@
-let pokemonList =[]
+let globalPokemonList =[]
 pokemonList=[
 {name:"Bulbasaur",height:0.7,types:["grass","poison"]},
 {name:"Charizard",height:1.7,types:["fire","flying"]},
 {name:"Parasect",height:1.4,types:["grass","bug"]}
 ]
 
-for (let i=0;i < pokemonList.length; i++){
-    text=pokemonList[i].name +" (height: "+ pokemonList[i].height+")."
-    if (pokemonList[i].height<1){
-      document.write(text+" It's a small pokemon "+"<br>");
-    }
-    else if (pokemonList[i].height > 1 && pokemonList[i].height < 1.5){
-      document.write(text+" It's an average size pokemon "+"<br>");
-    }
-    else{
-      document.write(text+" It's a big pokemon "+"<br>");
-    }
-}
+// for (let i=0;i < pokemonList.length; i++)
+// {
+//     text=pokemonList[i].name +" (height: "+ pokemonList[i].height+")."
+//     if (pokemonList[i].height<1){
+//       document.write(text+" It's a small pokemon "+"<br>");
+//     }
+//     else if (pokemonList[i].height > 1 && pokemonList[i].height < 1.5){
+//       document.write(text+" It's an average size pokemon "+"<br>");
+//     }
+//     else{
+//       document.write(text+" It's a big pokemon "+"<br>");
+//     }
+// }
+
+//Using a forEach() function instead of the for loop to iterate over 
+// pokemonList array in order to print the details of each one.
+
+
+// globalPokemonList.forEach(function (Pokemons){
+//   text=Pokemons.name +" (height: "+ Pokemons.height+")."
+//   if (Pokemons.height<1){
+//     document.write(text+" It's a small pokemon "+"<br>");
+//   }
+//   else if (Pokemons.height > 1 && Pokemons.height < 1.5){
+//     document.write(text+" It's an average size pokemon "+"<br>");
+//   }
+//   else{
+//     document.write(text+" It's a big pokemon "+"<br>");
+//   }
+// });
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name:"Bulbasaur",height:0.7,types:["grass","poison"]},
+    {name:"Charizard",height:1.7,types:["fire","flying"]},
+    {name:"Parasect",height:1.4,types:["grass","bug"]}
+    ]
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+pokemonRepository.getAll().forEach(function(Pokemons) {
+  let text = Pokemons.name + " (height: " + Pokemons.height + ").";
+  if (Pokemons.height < 1) {
+    document.write(text + " It's a small pokemon " + "<br>");
+  } else if (Pokemons.height > 1 && Pokemons.height < 1.5) {
+    document.write(text + " It's an average size pokemon " + "<br>");
+  } else {
+    document.write(text + " It's a big pokemon " + "<br>");
+  }
+});
